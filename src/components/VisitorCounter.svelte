ae9a455189e760153c983a296389db3d2b62b4de
nasircy <238443453+nasircy@users.noreply.github.com>
2026-08-16 00:06:15 +0800

    樣式: 訪客計數器改用 hitwebcounter

--- src/components/VisitorCounter.svelte
+++ src/components/VisitorCounter.svelte
@@ -1,29 +1,12 @@
 <div class="vcount">
   <nobr>
-    <table border="0" cellspacing="0" cellpadding="0">
-      <tbody>
-        <tr>
-          <td>
-            <a href="https://ctr.stylemap.co.jp/"
-              ><img
-                src="https://www.f-counter.net/ani1/67/1786805241/"
-                alt="無料アクセスカウンター"
-                border="0"
-                style="margin: 0px; padding: 0px; border: 0px; vertical-align: bottom" /></a
-            >
-          </td>
-          <td>
-            <a href="https://ctr.stylemap.co.jp/"
-              ><img
-                src="https://www.f-counter.net/ani2/67/1786805241/"
-                alt="無料アクセスカウンター"
-                border="0"
-                style="margin: 0px; padding: 0px; border: 0px; vertical-align: bottom" /></a
-            >
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <!-- hitwebcounter Code START -->
+    <a href="https://www.hitwebcounter.com/character-counter" target="_blank" rel="noopener" title="Total Characters">
+      <img src="https://www.hitwebcounter.com/counter/counter.php?page=21514902&amp;style=0001&amp;nbdigits=8&amp;type=page" alt="Total Characters" decoding="async" style="border:0;max-width:100%;height:auto;" />
+    </a>
+    <br />
+    <a href="https://www.hitwebcounter.com/" target="_blank" rel="noopener" style="font-family:Arial, Helvetica, sans-serif;font-size:10px;color:#636066;text-decoration:underline;font-weight:strong;">PageHits</a>
+    <!-- hitwebcounter Code END -->
   </nobr>
 </div>
 
